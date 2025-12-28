bcaddfbb6d381bdf7cf259bff8991e2e74030f04
hunghg255 <giahunghust@gmail.com>
2025-12-28 17:29:06 +0700

    chore: update

--- src/features/profile/data/certificates.ts
+++ src/features/profile/data/certificates.ts
@@ -6,8 +6,8 @@ export const CERTIFICATIONS: Certification[] = [
     issuer: "Emil Kowalski",
     issuerIconName: "Emil Kowalski",
     issueDate: "2024-08-31",
-    credentialID: "w9YXr-FMR8",
+    credentialID: "ee24b913-dbaf-470f-9758-50cd743d0cef",
     credentialURL:
-      "https://media.licdn.com/dms/image/v2/D562DAQGk3nRAzKb8hw/profile-treasury-image-shrink_1920_1920/profile-treasury-image-shrink_1920_1920/0/1730428760092?e=1754193600&v=beta&t=BZkwzDBJ5rBUzSl-R1C-nQ759bVl7ClaLVp2wc1mBFk",
+      "https://animations.dev/certificate/ee24b913-dbaf-470f-9758-50cd743d0cef",
   },
 ];
